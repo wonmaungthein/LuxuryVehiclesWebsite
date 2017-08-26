588169e7b3fdd8843ee67dd3079265e650ff7246
Won Maung Thein <wonmaungthein@gmail.com>
2017-08-26 19:51:14 +0100

    fixed a bit more together after the class

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
 const fs = require('fs');
+const formidable = require ("express-formidable");
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+app.listen(3000, function () {
+  console.log('Server is listening on port 3000. Ready to accept requests!');
+});
+
